067d9dd0206fbebb2469660f455afaf6240bb78f
giovanny <giovanny@hotmail.com.br>
2017-07-27 19:05:03 -0300

    Initial commit

---
+++ src/main/java/com/contaazul/marsexplorer/ExplorerApplication.java
@@ -0,0 +1,12 @@
+package com.contaazul.marsexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorerApplication.class, args);
+	}
+}

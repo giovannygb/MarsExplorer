e56bab92b256bcb3d96ab9fed5d0195c3b081ac0
giovanny <giovanny@hotmail.com.br>
2017-07-27 20:26:05 -0300

    Starting the robot at origin facing north

--- src/main/java/com/contaazul/marsexplorer/model/Robot.java
+++ src/main/java/com/contaazul/marsexplorer/model/Robot.java
@@ -1,5 +1,43 @@
 package com.contaazul.marsexplorer.model;
 
 public class Robot {
+	private Integer x;
+	private Integer y;
+	private String bearing;
+	
+	public Robot() {
+		this(0, 0, "N");
+	}
+	
+	public Robot(Integer x, Integer y, String bearing) {
+		super();
+		
+		this.x = x;
+		this.y = y;
+		this.bearing = bearing;
+	}
 
+	public Integer getX() {
+		return x;
+	}
+	
+	public void setX(Integer x) {
+		this.x = x;
+	}
+	
+	public Integer getY() {
+		return y;
+	}
+	
+	public void setY(Integer y) {
+		this.y = y;
+	}
+	
+	public String getBearing() {
+		return bearing;
+	}
+	
+	public void setBearing(String bearing) {
+		this.bearing = bearing;
+	}
 }
--- src/test/java/com/contaazul/marsexplorer/model/RobotTest.java
+++ src/test/java/com/contaazul/marsexplorer/model/RobotTest.java
@@ -1,12 +1,25 @@
 package com.contaazul.marsexplorer.model;
 
-import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.*;
 
 public class RobotTest {
+	private Robot robot;
+	
+	@Before
+	public void canInstantiateRobot() throws Exception {
+		robot = new Robot();
+	}
 	
 	@Test
-	public void canInstantiateRobot() throws Exception {
+	public void robotAtOriginFacingNorth() throws Exception {
 		Robot robot = new Robot();
+
+		assertThat(robot.getX(), is(0));
+		assertThat(robot.getY(), is(0));
+		assertThat(robot.getBearing(), is("N"));
 	}
 
 }

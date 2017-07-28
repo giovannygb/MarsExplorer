308df911f3becdfb8eed9d3dd2fd811ee8a25b33
giovanny <giovanny@hotmail.com.br>
2017-07-27 21:00:23 -0300

    Covering generated code

--- src/test/java/com/contaazul/marsexplorer/model/RobotTest.java
+++ src/test/java/com/contaazul/marsexplorer/model/RobotTest.java
@@ -16,6 +16,24 @@ public class RobotTest {
 	}
 	
 	@Test
+	public void robotSetX() throws Exception {
+		robot.setX(1);
+		assertThat(robot.getX(), is(1));
+	}
+	
+	@Test
+	public void robotSetY() throws Exception {
+		robot.setY(1);
+		assertThat(robot.getY(), is(1));
+	}
+	
+	@Test
+	public void robotSetBearingSouth() throws Exception {
+		robot.setBearing(Bearing.SOUTH);
+		assertThat(robot.getBearing(), is(Bearing.SOUTH));
+	}
+	
+	@Test
 	public void robotAtOriginFacingNorth() throws Exception {
 		assertThat(robot.getX(), is(0));
 		assertThat(robot.getY(), is(0));

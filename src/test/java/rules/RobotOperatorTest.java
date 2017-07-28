3bca796f7104bfe2e573082d33d52ad5ada6e920
giovanny <giovanny@hotmail.com.br>
2017-07-27 21:50:55 -0300

    Creating the RobotOperator class to operate the robot

---
+++ src/test/java/rules/RobotOperatorTest.java
@@ -0,0 +1,12 @@
+package rules;
+
+import org.junit.Test;
+
+public class RobotOperatorTest {
+	
+	@Test
+	public void canInstantiateRobotOperator() throws Exception {
+		RobotOperator robotOperator = new RobotOperator();
+	}
+
+}

6bf2a3d10811c1d74b5267098dce0c8a1a5cbe26
giovanny <giovanny@hotmail.com.br>
2017-07-28 18:48:09 -0300

    Creating RobotOperationMove class

---
+++ src/test/java/com/contaazul/marsexplorer/rules/operations/RobotOperationMoveTest.java
@@ -0,0 +1,14 @@
+package com.contaazul.marsexplorer.rules.operations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RobotOperationMoveTest {
+	
+	@Test
+	public void canInstantiateRobotOperationMove() throws Exception {
+		RobotOperationMove robotOperationMove = new RobotOperationMove();
+	}
+
+}

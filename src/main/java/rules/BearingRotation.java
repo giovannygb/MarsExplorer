38540c64a5b781ef8b20cec52371905ae03d1e92
giovanny <giovanny@hotmail.com.br>
2017-07-27 21:20:37 -0300

    Left of North is West

--- src/main/java/rules/BearingRotation.java
+++ src/main/java/rules/BearingRotation.java
@@ -1,5 +1,11 @@
 package rules;
 
+import enums.Bearing;
+
 public class BearingRotation {
+	
+	public Bearing leftOf(Bearing bearing) {
+		return Bearing.WEST;
+	}
 
 }
--- src/test/java/rules/BearingRotationTest.java
+++ src/test/java/rules/BearingRotationTest.java
@@ -1,10 +1,21 @@
 package rules;
 
-import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import enums.Bearing;
 
 public class BearingRotationTest {
+	private BearingRotation bearingRotation;
+	@Before
+	public void setupBearingRotation() throws Exception {
+		bearingRotation = new BearingRotation();
+	}
+	
 	@Test
-	public void canInstantiateBearingRotation() throws Exception {
-		BearingRotation bearingRotation = new BearingRotation();
+	public void leftOfNorthIsWest() throws Exception {
+		assertThat(bearingRotation.leftOf(Bearing.NORTH), is(Bearing.WEST));
 	}
 }

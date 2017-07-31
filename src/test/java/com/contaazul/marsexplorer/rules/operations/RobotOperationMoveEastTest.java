2bc28454734d3577cbfb26b38cca7d425f2befa9
giovanny <giovanny@hotmail.com.br>
2017-07-31 19:24:09 -0300

    MoveRobotEast implements and moves the robot east

--- src/test/java/com/contaazul/marsexplorer/rules/operations/RobotOperationMoveEastTest.java
+++ src/test/java/com/contaazul/marsexplorer/rules/operations/RobotOperationMoveEastTest.java
@@ -1,6 +1,10 @@
 package com.contaazul.marsexplorer.rules.operations;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Before;
+import org.junit.Test;
 
 import com.contaazul.marsexplorer.enums.Bearing;
 import com.contaazul.marsexplorer.model.Robot;
@@ -13,5 +17,18 @@ public class RobotOperationMoveEastTest {
 	public void setupRobotOperationMoveEast() throws Exception {
 		robotOperationMoveEast = new RobotOperationMoveEast();
 		robot = new Robot(0, 0, Bearing.EAST);
+	}
+	
+	@Test
+	public void moveRobotEastToX1Y0E() throws Exception {
+		robot = robotOperationMoveEast.execute(robot);
+
+		assertThatRobotIsAt(robot, 1, 0, Bearing.EAST);
+	}
+	
+	private static void assertThatRobotIsAt(Robot robot, Integer x, Integer y, Bearing bearing) {
+		assertThat(robot.getX(), is(x));
+		assertThat(robot.getY(), is(y));
+		assertThat(robot.getBearing(), is(bearing));
 	}
 }

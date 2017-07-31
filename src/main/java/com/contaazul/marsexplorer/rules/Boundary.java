c5a96f2382bdc6d483980af955fd82150fcac40a
giovanny <giovanny@hotmail.com.br>
2017-07-31 20:00:27 -0300

    Creating the object to validate the robot boundary condition

---
+++ src/main/java/com/contaazul/marsexplorer/rules/Boundary.java
@@ -0,0 +1,12 @@
+package com.contaazul.marsexplorer.rules;
+
+public class Boundary {
+	private Integer min;
+	private Integer max;
+	
+	public Boundary(Integer min, Integer max) {
+		super();
+		this.min = min;
+		this.max = max;
+	}
+}
---
+++ src/test/java/com/contaazul/marsexplorer/rules/BoundaryTest.java
@@ -0,0 +1,13 @@
+package com.contaazul.marsexplorer.rules;
+
+import org.junit.Test;
+
+public class BoundaryTest {
+	private Boundary boundary;
+	
+	@Test
+	public void setupBoundary() throws Exception {
+		boundary = new Boundary(0, 5);
+	}
+
+}

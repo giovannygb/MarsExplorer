d49e93ccd73b60724b2a15d0003949cf51ade282
giovanny <giovanny@hotmail.com.br>
2017-07-31 20:03:15 -0300

    Testing boundary generated code

--- src/main/java/com/contaazul/marsexplorer/rules/Boundary.java
+++ src/main/java/com/contaazul/marsexplorer/rules/Boundary.java
@@ -9,4 +9,20 @@ public class Boundary {
 		this.min = min;
 		this.max = max;
 	}
+
+	public Integer getMin() {
+		return min;
+	}
+
+	public void setMin(Integer min) {
+		this.min = min;
+	}
+
+	public Integer getMax() {
+		return max;
+	}
+
+	public void setMax(Integer max) {
+		this.max = max;
+	}
 }
--- src/test/java/com/contaazul/marsexplorer/rules/BoundaryTest.java
+++ src/test/java/com/contaazul/marsexplorer/rules/BoundaryTest.java
@@ -1,13 +1,30 @@
 package com.contaazul.marsexplorer.rules;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class BoundaryTest {
 	private Boundary boundary;
 	
-	@Test
+	@Before
 	public void setupBoundary() throws Exception {
 		boundary = new Boundary(0, 5);
 	}
-
+	
+	@Test
+	public void boundarySetMinValue() throws Exception {
+		boundary.setMin(1);
+		
+		assertThat(boundary.getMin(), is(1));
+	}
+	
+	@Test
+	public void boundarySetMaxValue() throws Exception {
+		boundary.setMax(1);
+		
+		assertThat(boundary.getMax(), is(1));
+	}
 }

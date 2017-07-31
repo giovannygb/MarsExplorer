145d95645fc684d3791626695c374c38d0c1c71c
giovanny <giovanny@hotmail.com.br>
2017-07-31 19:49:17 -0300

    Removing unecessary if statement

--- src/main/java/com/contaazul/marsexplorer/rules/operations/RobotOperationMove.java
+++ src/main/java/com/contaazul/marsexplorer/rules/operations/RobotOperationMove.java
@@ -1,18 +1,29 @@
 package com.contaazul.marsexplorer.rules.operations;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.contaazul.marsexplorer.enums.Bearing;
 import com.contaazul.marsexplorer.model.Robot;
 
 public class RobotOperationMove implements RobotOperation {
+	private Map<Bearing, RobotOperation> moveOperationDirection;
+	
+	public RobotOperationMove() {
+		Map<Bearing, RobotOperation> moveOperationDirection = new HashMap<>();
+		
+		moveOperationDirection.put(Bearing.NORTH, new RobotOperationMoveNorth());		
+		moveOperationDirection.put(Bearing.EAST, new RobotOperationMoveEast());		
+		moveOperationDirection.put(Bearing.SOUTH, new RobotOperationMoveSouth());		
+		moveOperationDirection.put(Bearing.WEST, new RobotOperationMoveWest());
+		
+		this.moveOperationDirection = Collections.unmodifiableMap(new HashMap<Bearing, RobotOperation>(moveOperationDirection));
+	}
+	
 	public Robot execute(Robot robot) {
-		if (robot.getBearing() == Bearing.NORTH) {
-			return new RobotOperationMoveNorth().execute(robot);
-		} else if (robot.getBearing() == Bearing.EAST) {
-			return new RobotOperationMoveEast().execute(robot);
-		} else if (robot.getBearing() == Bearing.SOUTH) {
-			return new RobotOperationMoveSouth().execute(robot);
-		} else {
-			return new RobotOperationMoveWest().execute(robot);
-		}
+		RobotOperation operation = moveOperationDirection.get(robot.getBearing());
+		
+		return operation.execute(robot);
 	}
 }

04d8c86724ddee0a558e1bb1eda6774b1b664980
giovanny <giovanny@hotmail.com.br>
2017-08-01 18:15:36 -0300

    Annotating class for rest access using spring

--- src/main/java/com/contaazul/marsexplorer/controller/RobotController.java
+++ src/main/java/com/contaazul/marsexplorer/controller/RobotController.java
@@ -1,7 +1,13 @@
 package com.contaazul.marsexplorer.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rest")
 public class RobotController {
-	public String execute(String command) {
+	
+	@PostMapping("/mars/{command}")
+	public String execute(@PathVariable String command) {
 		return "(0, 0, N)";
 	}
 }

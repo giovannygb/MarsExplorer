320318c7db8083b3c49010146876195a3cd0111d
giovanny <giovanny@hotmail.com.br>
2017-08-01 19:52:23 -0300

    Cleaning imports

--- src/test/java/com/contaazul/marsexplorer/rules/operations/RobotOperationMoveWestTest.java
+++ src/test/java/com/contaazul/marsexplorer/rules/operations/RobotOperationMoveWestTest.java
@@ -3,7 +3,6 @@ package com.contaazul.marsexplorer.rules.operations;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.contaazul.marsexplorer.enums.Bearing;
 import com.contaazul.marsexplorer.model.Robot;
 
 public class RobotOperationMoveWestTest {

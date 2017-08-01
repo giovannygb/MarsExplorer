ae4ef6cebe9d4f2b159b98d635ad82fbb63badea
giovanny <giovanny@hotmail.com.br>
2017-08-01 18:14:17 -0300

    Creating the execution method at the controller

--- src/main/java/com/contaazul/marsexplorer/controller/RobotController.java
+++ src/main/java/com/contaazul/marsexplorer/controller/RobotController.java
@@ -1,5 +1,7 @@
 package com.contaazul.marsexplorer.controller;
 
 public class RobotController {
-
+	public String execute(String command) {
+		return "(0, 0, N)";
+	}
 }
--- src/test/java/com/contaazul/marsexplorer/controller/RobotControllerTest.java
+++ src/test/java/com/contaazul/marsexplorer/controller/RobotControllerTest.java
@@ -1,14 +1,20 @@
 package com.contaazul.marsexplorer.controller;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
 
 public class RobotControllerTest {
 	private RobotController robotController;
 	
-	@Test
+	@Before
 	public void setupRobotController() throws Exception {
 		robotController = new RobotController();
+	}
+	
+	@Test
+	public void canExecuteCommand() throws Exception {
+		assertThat(robotController.execute(""), is("(0, 0, N)"));
 	}
 }

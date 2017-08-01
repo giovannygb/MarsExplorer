a73e3c8f228794888d81a72b8166fa09d78fc419
giovanny <giovanny@hotmail.com.br>
2017-08-01 18:25:05 -0300

    Annotating the class as a service

--- src/main/java/com/contaazul/marsexplorer/service/RobotOperatorService.java
+++ src/main/java/com/contaazul/marsexplorer/service/RobotOperatorService.java
@@ -1,7 +1,10 @@
 package com.contaazul.marsexplorer.service;
 
+import org.springframework.stereotype.Service;
+
 import com.contaazul.marsexplorer.model.Robot;
 
+@Service
 public class RobotOperatorService {
 	public Robot execute(String command) {
 		return new Robot();

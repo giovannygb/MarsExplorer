c06d09600f8951743b8913fc66270e68a7ee4f70
giovanny <giovanny@hotmail.com.br>
2017-08-02 19:23:56 -0300

    Delegating the responsability to parse the robot operation move command to its own class

--- src/main/java/com/contaazul/marsexplorer/rules/operations/RobotOperationMove.java
+++ src/main/java/com/contaazul/marsexplorer/rules/operations/RobotOperationMove.java
@@ -1,28 +1,17 @@
 package com.contaazul.marsexplorer.rules.operations;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-import com.contaazul.marsexplorer.enums.Bearing;
 import com.contaazul.marsexplorer.model.Robot;
+import com.contaazul.marsexplorer.rules.RobotOperationParser;
 
 public class RobotOperationMove implements RobotOperation {
-	private Map<Bearing, RobotOperation> moveOperationDirection;
+	private RobotOperationParser robotOperationParser;
 	
 	public RobotOperationMove() {
-		Map<Bearing, RobotOperation> moveOperationDirection = new HashMap<>();
-		
-		moveOperationDirection.put(Bearing.NORTH, new RobotOperationMoveNorth());		
-		moveOperationDirection.put(Bearing.EAST, new RobotOperationMoveEast());		
-		moveOperationDirection.put(Bearing.SOUTH, new RobotOperationMoveSouth());		
-		moveOperationDirection.put(Bearing.WEST, new RobotOperationMoveWest());
-		
-		this.moveOperationDirection = Collections.unmodifiableMap(new HashMap<Bearing, RobotOperation>(moveOperationDirection));
+		robotOperationParser = new RobotOperationParser();
 	}
 	
 	public Robot execute(Robot robot) {
-		RobotOperation operation = moveOperationDirection.get(robot.getBearing());
+		RobotOperation operation = robotOperationParser.get(robot.getBearing());
 		
 		return operation.execute(robot);
 	}
